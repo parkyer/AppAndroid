034bce1c129780dd581ba53097c1c60266017cf5
saariasba <saariasba@unal.edu.co>
2021-05-01 18:59:20 -0500

    Diseño de Contacto

--- app/src/main/java/com/arquitectura/parkyer/MainActivity.kt
+++ app/src/main/java/com/arquitectura/parkyer/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.arquitectura.parkyer
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.arquitectura.parkyer.microservicios.MicroServicioContacto
+import com.arquitectura.parkyer.views.Contacto
 
 class MainActivity : AppCompatActivity() {
     private val contacto =
@@ -11,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        contacto.obtenerMensajes()
+        //val intent = Intent(this,Contacto::class.java)
+        //startActivity(intent)
     }
 }
