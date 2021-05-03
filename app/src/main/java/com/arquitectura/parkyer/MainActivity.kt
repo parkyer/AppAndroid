d9a3aa791e076590b73523e89164fddd99bb782e
saariasba <saariasba@unal.edu.co>
2021-05-02 20:24:59 -0500

    Completo el menú

--- app/src/main/java/com/arquitectura/parkyer/MainActivity.kt
+++ app/src/main/java/com/arquitectura/parkyer/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.arquitectura.parkyer.microservicios.MicroServicioPerfil
 import com.arquitectura.parkyer.models.User
 import com.arquitectura.parkyer.views.Contacto
+import com.arquitectura.parkyer.views.Perfil
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this, Contacto::class.java)
+        val intent = Intent(this, Perfil::class.java)
         startActivity(intent)
     }
 }
